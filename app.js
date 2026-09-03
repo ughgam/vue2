dafaa9746ac94bb3f485e957c8916509e5cb4ac0
ughgam <sahuagam07@gmail.com>
2026-09-03 21:55:12 +0530

    same stuff in componenets

--- app.js
+++ app.js
@@ -1,21 +1,38 @@
-var app=new Vue({
-    el:"#app",
-    data:{
-        visitor_name:'',
-        visitor_message:'',
-        messages:[]
+Vue.component('mess_board',{
+    template: `
+        <div>and you?
+        <input type="text" v-model="visitor_name">
+        <br>and your thoughts?
+        <input type="text" v-model="visitor_message">
+        <button v-on:click="sayhi">Say Hi</button>
+
+        <ul>
+            <li v-for="message in messages">{{message["visitor_name"]}} said {{message['visitor_message']}}</li>
+        </ul>
+        </div>
+        
+    `,
+    data: function() {
+        return {
+            visitor_name:'',
+            visitor_message:'',
+            messages:[]
+        }
     },
     methods:{
-        sayhi:function(){
+        sayhi: function() {
             this.messages.push({"visitor_name":this.visitor_name, "visitor_message":this.visitor_message})
             this.visitor_name='',
             this.visitor_message=""
-            }
-        },
+        }
+    },
     computed:{
         count:function(){
             return this.messages.length;
         }
-        }
-    
+    }
+})
+
+var app=new Vue({
+    el:"#app"
 })
